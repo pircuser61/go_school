2efc2e7a7077d3080bfef92c85054957716545c6
Бойко Вячеслав Сергеевич <vsbojko1@mts.ru>
2020-05-19 09:36:09 +0300

    fix colors

--- internal/db/crud.go
+++ internal/db/crud.go
@@ -386,14 +386,14 @@ func CheckModuleUsage(c context.Context, pc *dbconn.PGConnection, name string) (
 	c, span := trace.StartSpan(c, "pg_write_context")
 	defer span.End()
 	l := make([]uuid.UUID, 0, 0)
-	q := `
-select pp.id from 
-	pipeliner.pipelines pp
-	join pipeliner.versions pv on pv.pipeline_id = pp.id
-where 
-	pp.deleted_at is NULL 
-and pv.content ?| 'pipeline,entrypoint''"
-`
+//	q := `
+//select pp.id from
+//	pipeliner.pipelines pp
+//	join pipeliner.versions pv on pv.pipeline_id = pp.id
+//where
+//	pp.deleted_at is NULL
+//and pv.content ?| 'pipeline,entrypoint''"
+//`
 
 		return l, nil
 }
