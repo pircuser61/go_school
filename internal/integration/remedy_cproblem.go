962b68632f6b535cb940784be017d632b5c1248d
Бойко Вячеслав Сергеевич <vsbojko1@mts.ru>
2020-10-28 15:39:39 +0300

    Remedy integration

--- internal/integration/remedy_cproblem.go
+++ internal/integration/remedy_cproblem.go
@@ -5,14 +5,15 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"gitlab.services.mts.ru/erius/pipeliner/internal/metrics"
-	"gitlab.services.mts.ru/erius/pipeliner/internal/script"
-	"gitlab.services.mts.ru/erius/pipeliner/internal/store"
-	"go.opencensus.io/trace"
 	"net/http"
 	"net/url"
 	"path"
 	"time"
+
+	"gitlab.services.mts.ru/erius/pipeliner/internal/metrics"
+	"gitlab.services.mts.ru/erius/pipeliner/internal/script"
+	"gitlab.services.mts.ru/erius/pipeliner/internal/store"
+	"go.opencensus.io/trace"
 )
 
 type RemedySendCreateProblem struct {
@@ -24,38 +25,38 @@ type RemedySendCreateProblem struct {
 }
 
 type RemedySendCreateProblemModel struct {
-	ExtID                    string    `json:"extID,omitempty"`
-	OperationID              string    `json:"operationID,omitempty"`
-	Source                   string    `json:"source,omitempty"`
-	Region                   string    `json:"region,omitempty"`
-	Status                   int       `json:"status,omitempty"`
-	Priority                 int       `json:"priority,omitempty"`
-	ClassificatorDescription string    `json:"classificatordescription,omitempty"`
-	ServiceImpactCls         string    `json:"serviceImpactCls,omitempty"`
-	SolutionCode             int       `json:"solutioncode,omitempty"`
-	FlagInvestment           int       `json:"flaginvestment,omitempty"`
-	ClosureCode              int       `json:"closurecode,omitempty"`
-	Description              string    `json:"description,omitempty"`
-	ClassificatorCause       string    `json:"classificatorcause,omitempty"`
-	ClassificatorSolution    string    `json:"classificatorsolution,omitempty"`
-	Solution                 string    `json:"solution,omitempty"`
-	ResponsibilityZone       string    `json:"responsibility_zone,omitempty"`
-	EventTime                time.Time `json:"eventtime,omitempty"`
-	FixTime                  time.Time `json:"fixtime,omitempty"`
-	Deadline                 time.Time `json:"deadline,omitempty"`
-	SolutionPlanTime         time.Time `json:"solutionplantime,omitempty"`
-	InitiatorLogin           string    `json:"initiator_login,omitempty"`
-	ExecutorLogin            string    `json:"executor_login,omitempty"`
-	ExecutorGroupID          string    `json:"executor_group_id,omitempty"`
-	SupervisorLogin          string    `json:"supervisor_login,omitempty"`
-	SupervisorGroupID        string    `json:"supervisor_group_id,omitempty"`
-	NENiossID                string    `json:"ne_nioss_id,omitempty"`
-	NESubsystem              string    `json:"ne_subsystem,omitempty"`
+	ExtID                    string       `json:"extID,omitempty"`
+	OperationID              string       `json:"operationID,omitempty"`
+	Source                   string       `json:"source,omitempty"`
+	Region                   string       `json:"region,omitempty"`
+	Status                   int          `json:"status,omitempty"`
+	Priority                 int          `json:"priority,omitempty"`
+	ClassificatorDescription string       `json:"classificatordescription,omitempty"`
+	ServiceImpactCls         string       `json:"serviceImpactCls,omitempty"`
+	SolutionCode             int          `json:"solutioncode,omitempty"`
+	FlagInvestment           int          `json:"flaginvestment,omitempty"`
+	ClosureCode              int          `json:"closurecode,omitempty"`
+	Description              string       `json:"description,omitempty"`
+	ClassificatorCause       string       `json:"classificatorcause,omitempty"`
+	ClassificatorSolution    string       `json:"classificatorsolution,omitempty"`
+	Solution                 string       `json:"solution,omitempty"`
+	ResponsibilityZone       string       `json:"responsibility_zone,omitempty"`
+	EventTime                time.Time    `json:"eventtime,omitempty"`
+	FixTime                  time.Time    `json:"fixtime,omitempty"`
+	Deadline                 time.Time    `json:"deadline,omitempty"`
+	SolutionPlanTime         time.Time    `json:"solutionplantime,omitempty"`
+	InitiatorLogin           string       `json:"initiator_login,omitempty"`
+	ExecutorLogin            string       `json:"executor_login,omitempty"`
+	ExecutorGroupID          string       `json:"executor_group_id,omitempty"`
+	SupervisorLogin          string       `json:"supervisor_login,omitempty"`
+	SupervisorGroupID        string       `json:"supervisor_group_id,omitempty"`
+	NENiossID                string       `json:"ne_nioss_id,omitempty"`
+	NESubsystem              string       `json:"ne_subsystem,omitempty"`
 }
 
 func NewRemedySendCreateProblem(remedyPath string, httpClient *http.Client) RemedySendCreateProblem {
 	return RemedySendCreateProblem{
-		Name:       "remedy_create_problem",
+		Name:       "remedy-create-problem",
 		Input:      make(map[string]string),
 		HttpClient: *httpClient,
 		Remedy:     remedyPath,
