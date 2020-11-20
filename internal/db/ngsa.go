cb346bb9cb9db7dbb40b215051f5bc5b9159b2e0
Сухоносик Дмитрий Сергеевич <dssukhon@mts.ru>
2020-11-20 16:48:37 +0300

    Add notification id generation

--- internal/db/ngsa.go
+++ internal/db/ngsa.go
@@ -3,28 +3,31 @@ package db
 import (
 	"context"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 func (db *PGConnection) ActiveAlertNGSA(c context.Context, sever int,
 	state, source, eventType, cause, addInf, addTxt, moID, specProb, notID, usertext, moi, moc string) error {
 	t := time.Now()
-	q := `INSERT INTO pipeliner.ngsa_alert(
-		state,
-		"perceivedSeverity", 
-		"eventSource", 
-		"eventTime", 
-		"eventType", 
-		"probableCause", 
-		"additionalInformation", 
-		"additionalText", 
-		"moIdentifier", 
-		"specificProblem", 
-		"notificationIdentifier", 
-		"userText", 
-		managedobjectinstance, 
-		managedobjectclass)
-	VALUES ($1, $2, $3, $4, $5, $6, $7,$8, $9, $10, $11, $12, $13, $14);`
-	_, err := db.Pool.Exec(c, q, state, sever, source, t, eventType, cause,
+	id := uuid.New().String()
+	q := `INSERT INTO pipeliner.ngsa_alert(id,
+                                 state,
+                                 "perceivedSeverity",
+                                 "eventSource",
+                                 "eventTime",
+                                 "eventType",
+                                 "probableCause",
+                                 "additionalInformation",
+                                 "additionalText",
+                                 "moIdentifier",
+                                 "specificProblem",
+                                 "notificationIdentifier",
+                                 "userText",
+                                 managedobjectinstance,
+                                 managedobjectclass)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15);`
+	_, err := db.Pool.Exec(c, q, id, state, sever, source, t, eventType, cause,
 		addInf, addTxt, moID, specProb, notID, usertext, moi, moc)
 
 	return err
