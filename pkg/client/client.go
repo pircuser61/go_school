163a233a5b44b2f960afea59ee90bb710cb3e8a5
Сметанкин Кирилл Игоревич <kismetan@mts.ru>
2021-03-09 18:29:38 +0300

    fix client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -4,56 +4,78 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"path"
-	"time"
+	"strconv"
 
-	"github.com/google/uuid"
+	"github.com/pkg/errors"
 )
 
-var PipelineRunFailed = errors.New("pipeline run failed")
+const (
+	PipelineRunFailed  = "pipeline run failed"
+	EmptyResponse      = "empty response"
+	CreateRequstError  = "can't create http request"
+	HTTPClientError    = "can't do http request"
+	MarshalJSONError   = "can't marshal json"
+	UnmarshalReadError = "can't read response body"
+	UnmarshalJSONError = "can't umarshal json"
+)
 
 type PipelinerConfig struct {
-	Host    string        `json:"host"`
-	User    string        `json:"user"`
-	Timeout time.Duration `json:"timeout"`
+	Host URL    `json:"host"`
+	User string `json:"user"`
+}
+
+type URL struct {
+	*url.URL
+}
+
+func (u *URL) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	stringURL := ""
+
+	err := unmarshal(&stringURL)
+	if err != nil {
+		return err
+	}
+
+	u.URL, err = url.Parse(stringURL)
+
+	return err
 }
 
 type PipelinerClient struct {
 	client *http.Client
 	user   string
-	host   string
+	host   *url.URL
 }
 
-func NewPipelinerClient(pc PipelinerConfig) (*PipelinerClient, error) {
-	cli := &http.Client{
-		Timeout: pc.Timeout,
-	}
-
+func NewPipelinerClient(pc PipelinerConfig, httpClient *http.Client) *PipelinerClient {
 	return &PipelinerClient{
-		client: cli,
+		client: httpClient,
 		user:   pc.User,
-		host:   pc.Host,
-	}, nil
+		host:   pc.Host.URL,
+	}
 }
 
-func (pc *PipelinerClient) RunPipelineSync(ctx context.Context, pid uuid.UUID, data interface{}) (map[string]interface{}, error) {
-	u, err := url.Parse(path.Join(pc.host, "api/pipeliner/v1/run", pid.String()+"?with_stop=true"))
-	if err != nil {
-		return nil, err
-	}
+func (pc *PipelinerClient) RunPipelineSync(ctx context.Context, pid fmt.Stringer, data interface{}) (map[string]interface{}, error) {
+	params := url.Values{}
+	params.Add("with_stop", strconv.FormatBool(true))
+
+	urlCopy := pc.host
+	urlCopy.Path = path.Join(pc.host.Path, "api/pipeliner/v1/run", pid.String())
+	urlCopy.RawQuery = params.Encode()
 
 	b, err := json.Marshal(data)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, MarshalJSONError)
 	}
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), bytes.NewBuffer(b))
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, urlCopy.String(), bytes.NewBuffer(b))
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, CreateRequstError)
 	}
 
 	// fixme extract "X-Request-Id" to variable
@@ -62,26 +84,29 @@ func (pc *PipelinerClient) RunPipelineSync(ctx context.Context, pid uuid.UUID, d
 
 	resp, err := pc.client.Do(req)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, HTTPClientError)
 	}
 	defer resp.Body.Close()
+
 	if resp.StatusCode != http.StatusOK {
-		return nil, PipelineRunFailed
+		return nil, errors.New(PipelineRunFailed)
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, UnmarshalReadError)
 	}
+
 	if len(body) == 0 {
-		return nil, http.ErrContentLength
+		return nil, errors.New(EmptyResponse)
 	}
 
 	result := make(map[string]interface{})
 
 	err = json.Unmarshal(body, &result)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, UnmarshalJSONError)
 	}
+
 	return result, nil
 }
