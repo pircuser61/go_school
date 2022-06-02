a46262bb3465e2cec25feabb5b4e76c03c2e06c8
ivmolotkov <ivmolotkov@mts.ru>
2022-06-02 11:37:30 +0300

    remove TODO

--- internal/entity/response-data.go
+++ internal/entity/response-data.go
@@ -74,9 +74,8 @@ type EriusFunctionList struct {
 }
 
 type EriusFunc struct {
-	X int `json:"x,omitempty"`
-	Y int `json:"y,omitempty"`
-	// TODO
+	X         int                  `json:"x,omitempty"`
+	Y         int                  `json:"y,omitempty"`
 	BlockType string               `json:"block_type" enums:"python3,go,internal,term,scenario" example:"python3"`
 	Title     string               `json:"title" example:"lock-bts"`
 	Input     []EriusFunctionValue `json:"input"`
