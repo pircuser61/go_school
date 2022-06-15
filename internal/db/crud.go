364d0729c0df4f6f9e142bc91dd9dc3d08914b74
Косяк Сергей Николаевич <snkosya1@mts.ru>
2022-06-15 15:05:48 +0300

    lint

--- internal/db/crud.go
+++ internal/db/crud.go
@@ -1831,7 +1831,7 @@ func compileGetTasksQuery(filters entity.TaskFilter) (q string, args []interface
 	if filters.SelectAs != nil {
 		if *filters.SelectAs == "approver" {
 			args = append(args, filters.CurrentUser)
-			q = fmt.Sprintf("%s AND approvers.content::json->'State'->approvers.step_name->'approvers'->$%d " +
+			q = fmt.Sprintf("%s AND approvers.content::json->'State'->approvers.step_name->'approvers'->$%d "+
 				"IS NOT NULL AND approvers.is_finished = FALSE", q, len(args))
 		}
 	} else {
