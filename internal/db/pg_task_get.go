5a555529d820fa65f390ad7a137554f63c2d0f88
rapetrin1 <rapetrin1@mts.ru>
2022-09-05 15:08:20 +0300

    add filter for carousel

--- internal/db/pg_task_get.go
+++ internal/db/pg_task_get.go
@@ -116,7 +116,7 @@ func compileGetTasksQuery(filters entity.TaskFilter) (q string, args []interface
 	}
 
 	if filters.ForCarousel != nil && *filters.ForCarousel {
-		q = fmt.Sprintf("%s AND (w.human_status='done' AND (now()::TIMESTAMP - w.finished_at::TIMESTAMP) < '3 days'", q)
+		q = fmt.Sprintf("%s AND ((w.human_status='done' AND (now()::TIMESTAMP - w.finished_at::TIMESTAMP) < '3 days')", q)
 		q = fmt.Sprintf("%s OR w.human_status = 'wait')", q)
 	}
 
