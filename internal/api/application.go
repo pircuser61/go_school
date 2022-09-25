a69d7b5e2b42e5adbf6f2bf556dbe1cd43b71c3f
Косяк Сергей Николаевич <snkosya1@mts.ru>
2022-09-26 01:54:33 +0300

    to api.swagger.yaml

--- internal/api/application.go
+++ internal/api/application.go
@@ -8,22 +8,13 @@ import (
 	"net/http"
 )
 
-func (ae *APIEnv) GetApplication(w http.ResponseWriter, req *http.Request) {
+func (ae *APIEnv) GetApplication(w http.ResponseWriter, req *http.Request, workNumber string) {
 	ctx, s := trace.StartSpan(req.Context(), "get_application")
 	defer s.End()
 
 	log := logger.GetLogger(ctx)
 
-	wn := req.URL.Query().Get("workNumber")
-	if wn == "" {
-		e := BadFiltersError
-		log.Error("no work number")
-		_ = e.sendError(w)
-
-		return
-	}
-
-	data, err := ae.DB.GetApplicationData(wn)
+	data, err := ae.DB.GetApplicationData(workNumber)
 	if err != nil {
 		e := UnknownError
 		log.Error(err)
@@ -42,20 +33,11 @@ func (ae *APIEnv) GetApplication(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func (ae *APIEnv) SetApplication(w http.ResponseWriter, req *http.Request) {
+func (ae *APIEnv) SetApplication(w http.ResponseWriter, req *http.Request, workNumber string) {
 	ctx, s := trace.StartSpan(req.Context(), "get_application")
 	defer s.End()
 
 	log := logger.GetLogger(ctx)
-
-	wn := req.URL.Query().Get("workNumber")
-	if wn == "" {
-		e := BadFiltersError
-		log.Error("no work number")
-		_ = e.sendError(w)
-
-		return
-	}
 
 	var data orderedmap.OrderedMap
 	defer req.Body.Close()
@@ -67,7 +49,7 @@ func (ae *APIEnv) SetApplication(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	err := ae.DB.SetApplicationData(wn, &data)
+	err := ae.DB.SetApplicationData(workNumber, &data)
 	if err != nil {
 		e := UnknownError
 		log.Error(err)
