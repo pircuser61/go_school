90c232e11b0d6a49e488109e5ae4c29b5fee0916
voidrad <sobugreye1@mts.ru>
2022-10-04 17:23:25 +0300

    fix starting pipelenes bu pipeline_id

--- internal/script/sd_application.go
+++ internal/script/sd_application.go
@@ -1,17 +1,9 @@
 package script
 
-import (
-	"errors"
-)
-
 type SdApplicationParams struct {
 	PipelineID string `json:"pipeline_id"`
 }
 
 func (a *SdApplicationParams) Validate() error {
-	if a.PipelineID == "" {
-		return errors.New("pipelineID is empty")
-	}
-
 	return nil
 }
