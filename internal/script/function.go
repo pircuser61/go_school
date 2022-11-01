faf13347e719580a03b0820203ba2db8c36242cc
Быков Сергей <svbyk11@mts.ru>
2022-11-01 16:17:49 +0400

    [JAP-392] Added function parameters mapping

--- internal/script/function.go
+++ internal/script/function.go
@@ -2,14 +2,57 @@ package script
 
 import "errors"
 
+type MappingParam map[string]MappingValue
+
+type MappingValue struct {
+	Description string `json:"description"`
+	Type        string `json:"type"`
+
+	Format     string         `json:"format,omitempty"`
+	Items      []MappingParam `json:"items,omitempty"`
+	Properties MappingParam   `json:"properties,omitempty"`
+
+	Value string `json:"value,omitempty"`
+}
+
 type ExecutableFunctionParams struct {
-	Name    string `json:"name"`
-	Version string `json:"version"`
+	Name    string       `json:"name"`
+	Version string       `json:"version"`
+	Mapping MappingParam `json:"mapping"`
 }
 
 func (a *ExecutableFunctionParams) Validate() error {
 	if a.Name == "" || a.Version == "" {
 		return errors.New("got no function name or version")
+	}
+
+	err := a.validateMapping(a.Mapping)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *ExecutableFunctionParams) validateMapping(mappingParam MappingParam) error {
+	if mappingParam != nil {
+		for _, mappingValue := range mappingParam {
+			if mappingValue.Type == "" || mappingValue.Description == "" {
+				return errors.New("type and description are required")
+			}
+
+			err := a.validateMapping(mappingValue.Properties)
+			if err != nil {
+				return err
+			}
+
+			for _, item := range mappingValue.Items {
+				err := a.validateMapping(item)
+				if err != nil {
+					return err
+				}
+			}
+		}
 	}
 
 	return nil
