ccae176e8f21fd5f6d4d6d3743f0af23f022678c
rapetrin1 <rapetrin1@mts.ru>
2022-11-07 12:13:07 +0300

    add params to execution node

--- internal/api/api.go
+++ internal/api/api.go
@@ -399,10 +399,10 @@ type ApproverParams struct {
 }
 
 // Approver type:
-//   - user - Single user
-//   - group - Approver group ID
-//   - head - Receiver's head
-//   - FromSchema - Selected by initiator
+//   * user - Single user
+//   * group - Approver group ID
+//   * head - Receiver's head
+//   * FromSchema - Selected by initiator
 type ApproverType string
 
 // Approver update params
@@ -682,6 +682,12 @@ type ExecutionParams struct {
 	// List of accessibility properties for forms
 	FormsAccessibility *[]FormsAccessibility `json:"forms_accessibility,omitempty"`
 
+	// Show action edit application in SD
+	IsEditable bool `json:"is_editable"`
+
+	// auto apply prev decision
+	RepeatPrevDecision bool `json:"repeat_prev_decision"`
+
 	// Execution SLA (in working hours)
 	Sla int `json:"sla"`
 
@@ -693,9 +699,9 @@ type ExecutionParams struct {
 }
 
 // Execution type:
-//   - user - Single user
-//   - group - Execution group ID
-//   - from_schema - Selected by initiator
+//  * user - Single user
+//  * group - Execution group ID
+//  * from_schema - Selected by initiator
 type ExecutionParamsType string
 
 // Executor update params
@@ -752,9 +758,9 @@ type FormChangelogItem struct {
 }
 
 // Form executor type:
-//   - User - Single user
-//   - Initiator - Process initiator
-//   - From_schema - Selected by initiator
+//   * User - Single user
+//   * Initiator - Process initiator
+//   * From_schema - Selected by initiator
 type FormExecutorType string
 
 // Form params
@@ -1041,8 +1047,8 @@ type UsedBy struct {
 }
 
 // Approver decision:
-//   - approved - approver approved block
-//   - rejected - approver rejected block
+//  * approved - approver approved block
+//  * rejected - approver rejected block
 type ApproverDecision string
 
 // Block type (language)
@@ -1084,8 +1090,8 @@ type EriusTaskResponse struct {
 }
 
 // Executor decision:
-//   - executed - executor executed block
-//   - rejected - executor rejected block
+//  * executed - executor executed block
+//  * rejected - executor rejected block
 type ExecutionDecision string
 
 // HttpError defines model for httpError.
@@ -1125,11 +1131,11 @@ type PipelineRename struct {
 }
 
 // Tag status:
-//   - 1 - Draft
-//   - 2 - Approved
-//   - 3 - Deleted
-//   - 4 - Rejected
-//   - 5 - On approve
+//  * 1 - Draft
+//  * 2 - Approved
+//  * 3 - Deleted
+//  * 4 - Rejected
+//  * 5 - On approve
 type ScenarioStatus int
 
 // Task human readable status
