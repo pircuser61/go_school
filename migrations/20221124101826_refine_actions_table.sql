e032d65c633b3d4165f04a4dafa53de870625b88
Зимин Максим Юрьевич <myzimin@mts.ru>
2022-11-25 13:03:35 +0300

    migration fix

--- migrations/20221124101826_refine_actions_table.sql
+++ migrations/20221124101826_refine_actions_table.sql
@@ -25,8 +25,8 @@ set id = case
      when id = 'rejected' then '55fe7832-9109-45b0-883b-cfacc25d14ca'
      when id = 'affirmate' then 'a747532c-8a9d-42c7-98cc-07a341ca41c6'
      when id = 'viewed' then 'cf75561b-965a-46d5-a806-b8d59d9bc69e'
-     when id ='informed' then '96cdb5f7-d9af-453d-9292-f9d87339a059'
-     when 'sign' then '43d16439-f7e3-4dbb-8431-3bd401f46d9b'
+     when id = 'informed' then '96cdb5f7-d9af-453d-9292-f9d87339a059'
+     when id = 'sign' then '43d16439-f7e3-4dbb-8431-3bd401f46d9b'
 end;
 
 alter table dict_action_names
