ae74dbf04ac4caedaaa2e87a282f34d02933e0ab
Зимин Максим Юрьевич <myzimin@mts.ru>
2022-11-25 15:31:56 +0300

    lint

--- internal/pipeline/sockets.go
+++ internal/pipeline/sockets.go
@@ -3,7 +3,7 @@ package pipeline
 const (
 	DefaultSocketID = "default"
 
-	//nolint:deadcode //used for tests
+	//nolint:varcheck //used for tests
 	rejectedSocketID       = "rejected"
 	editAppSocketID        = "edit_app"
 	executedSocketID       = "executed"
