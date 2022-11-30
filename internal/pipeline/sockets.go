9cf4be7c9d8c1d950535d58552df6ab250b6b75e
Зимин Максим Юрьевич <myzimin@mts.ru>
2022-11-30 17:18:47 +0300

    fixed action name

--- internal/pipeline/sockets.go
+++ internal/pipeline/sockets.go
@@ -5,7 +5,7 @@ const (
 
 	//nolint:varcheck,deadcode //used in tests
 	rejectedSocketID       = "rejected"
-	editAppSocketID        = "edit_app"
+	editAppSocketID        = "send_edit_app"
 	executedSocketID       = "executed"
 	notExecutedSocketID    = "not_executed"
 	requestAddInfoSocketID = "req_add_info"
