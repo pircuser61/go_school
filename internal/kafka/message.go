6cfdb3a4ee4568184e5a8caa4c7db5a8ebafdfae
Быков Сергей <svbyk11@mts.ru>
2022-12-07 23:59:20 +0400

    [JAP-400] deleted SystemStand field

--- internal/kafka/message.go
+++ internal/kafka/message.go
@@ -7,7 +7,6 @@ type RunnerOutMessage struct {
 	FunctionMapping map[string]interface{} `json:"function_mapping"`
 	FunctionName    string                 `json:"function_name"`
 	RetryPolicy     string                 `json:"retry_policy"`
-	SystemStand     string                 `json:"system_stand"`
 }
 
 type RunnerInMessage struct {
