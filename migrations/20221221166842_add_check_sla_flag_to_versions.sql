1b058bb63c35bf35c5e5359785fe7b1c3db9a07d
rapetrin1 <rapetrin1@mts.ru>
2022-12-21 18:41:16 +0300

    sla 0

--- migrations/20221221166842_add_check_sla_flag_to_versions.sql
+++ migrations/20221221166842_add_check_sla_flag_to_versions.sql
@@ -1,7 +1,37 @@
 -- +goose Up
 -- +goose StatementBegin
 
-select distinct work_id from variable_storage limit 1;
+create or replace function add_check_sla_true_versions()
+    returns void
+    language plpgsql
+as $function$
+declare
+    versions uuid[] := array(select id from versions);
+    step_names varchar[];
+    v_id uuid;
+    s_name varchar;
+begin
+    foreach v_id IN ARRAY versions
+        loop
+            step_names = array(
+                select jsonb_object_keys(content -> 'pipeline' -> 'blocks')
+                from versions
+                where id = v_id
+            );
+
+            foreach s_name IN ARRAY step_names
+                loop
+                    if s_name like 'execution%' or s_name like 'approver%' then
+                        update versions
+                        set content = jsonb_set(content, array['pipeline', 'blocks', s_name, 'params', 'check_sla']::varchar[], 'true'::jsonb, true)
+                        where id = v_id;
+                    end if;
+                end loop;
+        end loop;
+end
+$function$;
+
+select * from add_check_sla_true_versions();
 -- +goose StatementEnd
 
 -- +goose Down
@@ -21,8 +51,7 @@ begin
             step_names = array(
                 select jsonb_object_keys(content -> 'pipeline' -> 'blocks')
                 from versions
-                where id = v_id and deleted_at is null and
-                    jsonb_typeof(content -> 'pipeline' -> 'blocks') = 'object'
+                where id = v_id
             );
 
             foreach s_name IN ARRAY step_names
