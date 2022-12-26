e4811561bd7ea3d454c7b2a7d1de7ebdd3d7edc1
Зимин Максим Юрьевич <myzimin@mts.ru>
2022-12-26 08:14:31 +0300

    added missing comments

--- migrations/20221226080033_add_missing_comments_for_tables.sql
+++ migrations/20221226080033_add_missing_comments_for_tables.sql
@@ -1,9 +1,27 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+comment on schema public is 'Детальные данные о сценариях Jocasta.';
+
+comment on column works.human_status is 'Статус заявки для ServiceDesk.';
+comment on column works.child_id is 'Уникальный UUID идентификатор дочерней заявки.';
+comment on column works.run_context is 'Контекст исполнения заявки.';
+
+comment on column dict_approve_statuses.title is 'Текст статуса пользовательского действия.';
+
+comment on column dict_approve_statuses.created_at is 'Поле для аудита. Дата создания записи в таблице.';
+comment on column dict_approve_statuses.deleted_at is 'Поле для аудита. Дата удаления записи в таблице.';
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+comment on schema public is null;
+
+comment on column works.human_status is null;
+comment on column works.child_id is null;
+comment on column works.run_context is null;
+
+comment on column dict_approve_statuses.title is null;
+
+comment on column dict_approve_statuses.created_at is null;
+comment on column dict_approve_statuses.deleted_at is null;
 -- +goose StatementEnd
