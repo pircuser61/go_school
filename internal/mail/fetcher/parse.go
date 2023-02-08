1d7a619736bc1a4250212165d51f9f5e9aa45dd8
rapetrin1 <rapetrin1@mts.ru>
2023-02-08 13:35:36 +0300

    fix comment

--- internal/mail/fetcher/parse.go
+++ internal/mail/fetcher/parse.go
@@ -256,7 +256,7 @@ LOOP:
 	}
 
 	pb.Body = strings.Replace(body, startLine, "", 1)
-	pb.Body = strings.Replace(pb.Body,"\n","",-1)
+	pb.Body = strings.Replace(pb.Body, "\n", "", -1)
 	pb.Body = strings.TrimSpace(pb.Body)
 	pb.Attachments = attachments
 
