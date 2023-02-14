97863294cb44dd66a200ab21c218f44b3f751aec
Oleg Svetovidov <opsveto1@mts.ru>
2023-02-14 14:25:47 +0300

    mock add

--- internal/test/mock.go
+++ internal/test/mock.go
@@ -928,6 +928,10 @@ type MockDB struct {
 	pipelines []entity.EriusScenario
 }
 
+func (_m *MockDB) GetMergedVariableStorage(ctx context.Context, workId string, blockIds []string) (*store.VariableStore, error) {
+	return nil, nil
+}
+
 func (_m *MockDB) GetBlocksOutputs(ctx context.Context, blockId string) (entity.BlockOutputs, error) {
 	return nil, nil
 }
