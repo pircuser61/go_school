22476354b422509413bd450e5f1e705a863b6cc4
kiinyakin <kiinyakin@mts.ru>
2023-03-28 13:49:37 +0300

    Added pgcon method to get works in given timerange

--- internal/db/crud.go
+++ internal/db/crud.go
@@ -22,6 +22,7 @@ import (
 	"github.com/jackc/pgx/v4/pgxpool"
 	"gitlab.services.mts.ru/jocasta/pipeliner/internal/configs"
 	"gitlab.services.mts.ru/jocasta/pipeliner/internal/entity"
+	"gitlab.services.mts.ru/jocasta/pipeliner/internal/pipeline"
 	"gitlab.services.mts.ru/jocasta/pipeliner/internal/store"
 )
 
@@ -2890,6 +2891,58 @@ func (db *PGCon) RemoveExternalSystem(ctx context.Context, versionID, systemID s
 	return nil
 }
 
-func (db *PGCon) GetWorksForUserWithGivenTimeRange() {
+const (
+	recipientSDFormKey = "recipient"
+)
 
+func (db *PGCon) GetWorksForUserWithGivenTimeRange(ctx context.Context, hours int, login, versionID string) ([]*entity.EriusTask, error) {
+	ctx, span := trace.StartSpan(ctx, "get_works_for_user_with_given_time_range")
+	defer span.End()
+
+	// nolint:gocritic
+	// language=PostgreSQL
+	query := fmt.Sprintf(`WITH works_cte as (select w.id work_id,
+                          w.author work_author,
+                          (vs.content -> 'State' -> vs.step_name -> 'application_body' -> %s ->>
+                           'username') work_recipient,
+                          w.work_number work_number
+                   from works w
+                            join variable_storage vs on w.id = vs.work_id
+                   where vs.step_type = 'servicedesk_application'
+                     and w.human_status in ($1, $2, $3, $4, $5)
+                     and w.started_at > now() - interval '1 hour' * $6
+                     and w.version_id = $7
+                     and w.parent_work_id is null)
+				   select work_id, work_author, work_number
+				   from works_cte
+				   where works_cte.work_recipient = $8
+				   or (works_cte.work_recipient is null and works_cte.work_author = $8)`,
+		recipientSDFormKey)
+
+	rows, queryErr := db.Connection.Query(ctx, query, pipeline.StatusDone, pipeline.StatusExecution, pipeline.StatusApproved, pipeline.StatusApprovement, pipeline.StatusWait, hours, versionID, login)
+	if queryErr != nil {
+		return nil, queryErr
+	}
+	defer rows.Close()
+
+	works := make([]*entity.EriusTask, 0, 0)
+
+	for rows.Next() {
+		var work entity.EriusTask
+
+		scanErr := rows.Scan(
+			&work.ID,
+			&work.Author,
+			&work.WorkNumber,
+		)
+		if scanErr != nil {
+			return nil, scanErr
+		}
+	}
+
+	if rowsErr := rows.Err(); rowsErr != nil {
+		return nil, rowsErr
+	}
+
+	return works, nil
 }
