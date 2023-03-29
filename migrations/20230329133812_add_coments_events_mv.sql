027f8f78ed7a9b972e9e08ccd8b010dcf08cd44e
kiinyakin <kiinyakin@mts.ru>
2023-03-29 13:44:31 +0300

    Added comments to mv events

---
+++ migrations/20230329133812_add_coments_events_mv.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+COMMENT ON TABLE events IS 'Вывод всех событий по ноде построчно';
+COMMENT ON COLUMN events.step_id IS 'Уникальный номер каждого шага';
+COMMENT ON COLUMN events."user" IS 'Пользователь совершивший событие';
+COMMENT ON COLUMN events.log_type IS 'Указывает тип лога';
+COMMENT ON COLUMN events.event_body IS 'Указывает, что именно сделал пользователь';
+COMMENT ON COLUMN events.created_at IS 'Дата, когда событие появилось.';
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+COMMENT ON TABLE events IS NULL ;
+COMMENT ON COLUMN events.step_id IS NULL ;
+COMMENT ON COLUMN events."user" IS NULL ;
+COMMENT ON COLUMN events.log_type IS NULL ;
+COMMENT ON COLUMN events.event_body IS NULL ;
+COMMENT ON COLUMN events.created_at IS NULL ;
+-- +goose StatementEnd
