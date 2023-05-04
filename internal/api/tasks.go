af909a033b6d2b3f5d1733369c941374a268cc05
rapetrin1 <rapetrin1@mts.ru>
2023-05-04 10:48:39 +0300

    add delegations

--- internal/api/tasks.go
+++ internal/api/tasks.go
@@ -342,9 +342,7 @@ func (ae *APIEnv) GetTasks(w http.ResponseWriter, req *http.Request, params GetT
 		delegations = delegations[:0]
 	}
 
-	getDelegatesFor := []string{filters.CurrentUser}
-
-	users := delegations.GetUserInArrayWithDelegators(getDelegatesFor)
+	users := delegations.GetUserInArrayWithDelegators([]string{filters.CurrentUser})
 
 	if filters.ProcessingLogins != nil && len(*filters.ProcessingLogins) > 0 {
 		delegations, err = ae.HumanTasks.GetDelegationsByLogins(ctx, *filters.ProcessingLogins)
@@ -356,18 +354,6 @@ func (ae *APIEnv) GetTasks(w http.ResponseWriter, req *http.Request, params GetT
 		}
 
 		users = delegations.GetUserInArrayWithDelegators(*filters.ProcessingLogins)
-	}
-
-	if filters.InitiatorLogins != nil && len(*filters.InitiatorLogins) > 0 {
-		delegations, err = ae.HumanTasks.GetDelegationsByLogins(ctx, *filters.InitiatorLogins)
-		if err != nil {
-			e := GetDelegationsError
-			log.Error(e.errorMessage(err))
-			_ = e.sendError(w)
-			return
-		}
-
-		users = append(users, delegations.GetUserInArrayWithDelegators(*filters.InitiatorLogins)...)
 	}
 
 	resp, err := ae.DB.GetTasks(ctx, filters, users)
