35638ddd9490b807638caf3478684108829a9788
rapetrin1 <rapetrin1@mts.ru>
2023-07-07 11:00:29 +0300

    fix review

--- migrations/20230706153156_replace_ssp_person.sql
+++ migrations/20230706153156_replace_ssp_person.sql
@@ -1,12 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
 UPDATE versions
-    SET content = REPLACE(content::text, '"type": "SsoPerson"', '"type": "object", "format": "SsoPerson"');
+    SET content = REPLACE(content::text, '"type": "SsoPerson"', '"type": "object", "format": "SsoPerson"')::jsonb;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 UPDATE versions
-    SET content = REPLACE(content::text, '"type": "object", "format": "SsoPerson"', '"type": "SsoPerson"');
+    SET content = REPLACE(content::text, '"type": "object", "format": "SsoPerson"', '"type": "SsoPerson"')::jsonb;
 
 -- +goose StatementEnd
