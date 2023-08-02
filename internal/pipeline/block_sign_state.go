583cdfa13eb91bb245c18095db2b50994e822e7a
Косяк Сергей Николаевич <snkosya1@mts.ru>
2023-08-02 14:52:07 +0300

    refactor set decision for sign node

--- internal/pipeline/block_sign_state.go
+++ internal/pipeline/block_sign_state.go
@@ -37,6 +37,55 @@ type SignData struct {
 	SignerGroupName string `json:"signer_group_name,omitempty"`
 }
 
+func (s *SignData) handleAnyOfDecision(login string, params *SignSignatureParams) {
+	s.Decision = &params.Decision
+	s.Comment = &params.Comment
+	s.ActualSigner = &login
+
+	var signingLogEntry = SignLogEntry{
+		Login:     login,
+		Decision:  params.Decision,
+		Comment:   params.Comment,
+		CreatedAt: time.Now(),
+	}
+
+	s.SignLog = append(s.SignLog, signingLogEntry)
+}
+
+func (s *SignData) handleAllOfDecision(login string, params *SignSignatureParams) error {
+	for i := 0; i < len(s.SignLog); i++ {
+		entry := s.SignLog[i]
+		if entry.Login == login {
+			return errors.New(fmt.Sprintf("decision of user %s is already set", login))
+		}
+	}
+
+	var signingLogEntry = SignLogEntry{
+		Login:     login,
+		Decision:  params.Decision,
+		Comment:   params.Comment,
+		CreatedAt: time.Now(),
+	}
+
+	s.SignLog = append(s.SignLog, signingLogEntry)
+
+	var overallDecision SignDecision
+
+	switch params.Decision {
+	case SignDecisionRejected:
+		overallDecision = SignDecisionRejected
+	case SignDecisionError:
+		overallDecision = SignDecisionError
+	default:
+		if len(s.SignLog) == len(s.Signers) {
+			overallDecision = SignDecisionSigned
+		}
+	}
+
+	s.Decision = &overallDecision
+	return nil
+}
+
 func (s *SignData) SetDecision(login string, params *SignSignatureParams) error {
 	_, signerFound := s.Signers[login]
 	if !signerFound {
@@ -62,51 +111,13 @@ func (s *SignData) SetDecision(login string, params *SignSignatureParams) error 
 	}
 
 	if signingRule == script.AnyOfSigningRequired {
-		s.Decision = &params.Decision
-		s.Comment = &params.Comment
-		s.ActualSigner = &login
-
-		var signingLogEntry = SignLogEntry{
-			Login:     login,
-			Decision:  params.Decision,
-			Comment:   params.Comment,
-			CreatedAt: time.Now(),
-		}
-
-		s.SignLog = append(s.SignLog, signingLogEntry)
+		s.handleAnyOfDecision(login, params)
 	}
 
 	if signingRule == script.AllOfSigningRequired {
-		for i := 0; i < len(s.SignLog); i++ {
-			entry := s.SignLog[i]
-			if entry.Login == login {
-				return errors.New(fmt.Sprintf("decision of user %s is already set", login))
-			}
+		if err := s.handleAllOfDecision(login, params); err != nil {
+			return err
 		}
-
-		var signingLogEntry = SignLogEntry{
-			Login:     login,
-			Decision:  params.Decision,
-			Comment:   params.Comment,
-			CreatedAt: time.Now(),
-		}
-
-		s.SignLog = append(s.SignLog, signingLogEntry)
-
-		var overallDecision SignDecision
-
-		switch params.Decision {
-		case SignDecisionRejected:
-			overallDecision = SignDecisionRejected
-		case SignDecisionError:
-			overallDecision = SignDecisionError
-		default:
-			if len(s.SignLog) == len(s.Signers) {
-				overallDecision = SignDecisionSigned
-			}
-		}
-
-		s.Decision = &overallDecision
 	}
 
 	return nil
