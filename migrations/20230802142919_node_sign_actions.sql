da490d3826ac65faf8adc70cf69653c6d2de560f
Косяк Сергей Николаевич <snkosya1@mts.ru>
2023-08-02 14:41:30 +0300

    migration

---
+++ migrations/20230802142919_node_sign_actions.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+-- +goose StatementBegin
+insert into pipeliner.public.dict_actions(id, title, is_public, comment_enabled, attachments_enabled)
+values ('sign_sign', 'Подписать', true, false, false),
+       ('sign_reject', 'Отклонить', true, true, false);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+delete
+from pipeliner.public.dict_actions
+where id in ('sign_sign', 'sign_reject')
+-- +goose StatementEnd
