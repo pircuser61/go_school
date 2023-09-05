c18678b210b9bdde7617cd851bd5afc3cf28f9f9
sobugreye1 <sobugreye1@mts.ru>
2023-09-05 10:49:25 +0300

    delete where in migration

--- migrations/20230905100917_change_sign_exit_attachement.sql
+++ migrations/20230905100917_change_sign_exit_attachement.sql
@@ -2,14 +2,12 @@
 -- +goose StatementBegin
 Update versions
 set content = replace(content::text, '"attachments": {"type": "array", "items": {"type": "string"}, "description": "signed files"}',
-    '"attachments": {"type": "array", "items": {"type": "object", "properties":{"file_id": {"type": "string", "description": "file id in file Registry"}, "external_link": {"type": "string", "description": "link to file in another system"}}}, "description": "signed files"}')::jsonb
-where content::text ilike '%"attachments": {"type": "array", "items": {"type": "string"}, "description": "signed files"}%';
+    '"attachments": {"type": "array", "items": {"type": "object", "properties":{"file_id": {"type": "string", "description": "file id in file Registry"}, "external_link": {"type": "string", "description": "link to file in another system"}}}, "description": "signed files"}')::jsonb;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 Update versions
 set content = replace(content::text, '"attachments": {"type": "array", "items": {"type": "object", "properties":{"file_id": {"type": "string", "description": "file id in file Registry"}, "external_link": {"type": "string", "description": "link to file in another system"}}}, "description": "signed files"}',
-                      '"attachments": {"type": "array", "items": {"type": "string"}, "description": "signed files"}')::jsonb
-where content::text ilike '%"attachments": {"type": "array", "items": {"type": "object", "properties":{"file_id": {"type": "string", "description": "file id in file Registry"}, "external_link": {"type": "string", "description": "link to file in another system"}}}, "description": "signed files"}%';
+                      '"attachments": {"type": "array", "items": {"type": "string"}, "description": "signed files"}')::jsonb;
 -- +goose StatementEnd
