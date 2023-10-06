cb095c57bdb354845b0a1391a6cfbfe1030c2afb
sobugreye1 <sobugreye1@mts.ru>
2023-10-06 17:12:02 +0300

    add show_executor to swagger

--- internal/api/api.go
+++ internal/api/api.go
@@ -1004,7 +1004,7 @@ type ExecutionParams struct {
 	// Execution rework SLA (in working hours)
 	ReworkSla int `json:"rework_sla"`
 
-	// flag to hide executor
+	// flag to show executor
 	ShowExecutor *bool `json:"show_executor,omitempty"`
 
 	// Execution SLA (in working hours)
