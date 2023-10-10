bec37c30fde8e4277d0a87b14d91910041162874
Косяк Сергей Николаевич <snkosya1@mts.ru>
2023-10-10 14:17:25 +0300

    to map

--- internal/entity/task.go
+++ internal/entity/task.go
@@ -209,6 +209,7 @@ var (
 		TaskUpdateActionSign:                  {},
 		TaskUpdateActionFinishTimer:           {},
 		TaskUpdateActionFuncSLAExpired:        {},
+		TaskUpdateActionSignChangeWorkStatus:  {},
 	}
 )
 
