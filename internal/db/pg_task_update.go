42d85ec128cf2b591f9ec1654257b10d29641bb6
Roman <petrinfedor35@gmail.com>
2023-10-11 15:29:06 +0300

    fix

--- internal/db/pg_task_update.go
+++ internal/db/pg_task_update.go
@@ -227,7 +227,7 @@ func (db *PGCon) UpdateBlockVariablesInOthers(ctx c.Context, taskId string, valu
 	for varName := range values {
 		const q = `
 		UPDATE variable_storage 
-		SET content = jsonb_set(content, array['State', 'Values', $1]::varchar[], $2::jsonb, false)
+		SET content = jsonb_set(content, array['Values', $1]::varchar[], $2::jsonb, false)
 		WHERE work_id = $3`
 
 		_, err := db.Connection.Exec(ctx, q, varName, values[varName], taskId)
