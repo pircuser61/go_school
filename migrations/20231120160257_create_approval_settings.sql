53ea1b9b5a3121bf806eb6f9bc993001a93467b8
rapetrin1 <rapetrin1@mts.ru>
2023-11-20 14:51:59 +0300

    list approval

--- migrations/20231120160257_create_approval_settings.sql
+++ migrations/20231120160257_create_approval_settings.sql
@@ -2,14 +2,15 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS version_approval_lists(
     id uuid not null primary key ,
-    version_id uuid not null references versions(id),
+    version_id uuid references versions(id),
     name varchar not null default '',
-    steps character varying[] default '{}'::character varying[] not null,
+    steps character varying[] default '{}'::character varying[],
     context_mapping jsonb default '{}'::jsonb not null,
     forms_mapping jsonb default '{}'::jsonb not null,
-    created_at timestamp with time zone not null default now,
+    created_at timestamp with time zone not null default now(),
     deleted_at timestamp with time zone
 );
+
 -- +goose StatementEnd
 
 -- +goose Down
