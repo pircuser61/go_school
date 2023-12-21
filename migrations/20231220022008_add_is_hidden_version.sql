a4ffc8c88b11be82f496c20e471286f2410ba4f9
Danil Zotin <dnzotin@mts.ru>
2023-12-21 16:46:33 +0300

    added more consistency with other migrations

--- migrations/20231220022008_add_is_hidden_version.sql
+++ migrations/20231220022008_add_is_hidden_version.sql
@@ -1,14 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE versions
-ADD is_hidden boolean;
-
-ALTER TABLE versions ALTER COLUMN is_hidden
-SET DEFAULT false;
+ADD COLUMN is_hidden boolean IF NOT EXISTS DEFAULT false;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 ALTER TABLE versions
-DROP COLUMN is_hidden;
+DROP COLUMN IF EXISTS is_hidden;
 -- +goose StatementEnd
