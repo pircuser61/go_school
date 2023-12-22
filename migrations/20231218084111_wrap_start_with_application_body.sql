c651ce9ea3a1f276fdeb824bf42c68e28b12b4bf
vapoliak <vapoliak@mts.ru>
2023-12-22 09:52:52 +0300

    refactor: Add nested object mapping update

--- migrations/20231218084111_wrap_start_with_application_body.sql
+++ migrations/20231218084111_wrap_start_with_application_body.sql
@@ -5,6 +5,36 @@ CREATE TABLE versions_21122023 AS (SELECT * FROM pipeliner.public.versions);
 CREATE TABLE variable_storage_21122023 AS (SELECT * FROM pipeliner.public.variable_storage);
 
 
+CREATE OR REPLACE FUNCTION jsonb_replace_obj_mapping(obj jsonb, path varchar[], input_v_ids uuid) RETURNS jsonb
+    language plpgsql AS $function$
+DECLARE
+    step_mappings varchar[];
+    s_map varchar;
+BEGIN
+    step_mappings = array(SELECT jsonb_object_keys(obj));
+
+    FOREACH s_map IN ARRAY step_mappings
+        loop
+            if obj -> s_map ->> 'value' LIKE 'start_0%' then
+                UPDATE pipeliner.public.versions v
+                SET "content" = jsonb_set("content"::jsonb,
+                    array_cat(path, array[s_map, 'value']::varchar[]),
+                    jsonb_build_array(concat(
+                        left(obj -> s_map ->> 'value', 8),
+                        'application_body.',
+                        substring(obj -> s_map ->> 'value', 9, length(obj -> s_map ->> 'value')))) -> 0,
+                    false)
+                WHERE v.id = input_v_ids;
+            end if;
+
+            if obj -> s_map -> 'properties' IS NOT NULL then
+                SELECT jsonb_replace_obj_mapping(
+                obj -> s_map -> 'properties',
+                    array_cat(path, array[s_map, 'properties']::varchar[]),
+                    input_v_ids);
+            end if;
+        end loop;
+END $function$;
 
 CREATE OR REPLACE FUNCTION add_app_body_prefix_mapping(input_v_ids uuid) RETURNS void
     language plpgsql
@@ -12,48 +42,31 @@ AS $function$
 DECLARE
     step_names varchar[];
     s_name varchar;
-    s_mappings varchar[];
-    s_map varchar;
-    str_to_update varchar;
 BEGIN
     step_names = array(
         SELECT jsonb_object_keys(content -> 'pipeline' -> 'blocks')
         FROM pipeliner.public.versions
-        WHERE id = input_v_ids AND deleted_at IS NULL
-            AND jsonb_typeof(content -> 'pipeline' -> 'blocks') = 'object');
+        WHERE id = input_v_ids);
 
     FOREACH s_name IN ARRAY step_names
         LOOP
-            s_mappings = array(
-                SELECT jsonb_object_keys(content -> 'pipeline' -> 'blocks' -> s_name -> 'params' -> 'mapping')
-                FROM pipeliner.public.versions
-                WHERE id = input_v_ids AND deleted_at IS NULL
-                    AND jsonb_typeof(content -> 'pipeline' -> 'blocks' -> s_name -> 'params' -> 'mapping') = 'object');
-
-            FOREACH s_map IN ARRAY s_mappings
-                LOOP
-                    str_to_update = (
-                        SELECT content -> 'pipeline' -> 'blocks' -> s_name -> 'params' -> 'mapping' -> s_map ->> 'value'
+            SELECT jsonb_replace_obj_mapping(
+                (
+                        SELECT content -> 'pipeline' -> 'blocks' -> s_name -> 'params' -> 'mapping'
                         FROM pipeliner.public.versions
-                        WHERE id = input_v_ids AND deleted_at IS NULL);
-
-                    IF str_to_update LIKE 'start_0%' THEN
-                        UPDATE pipeliner.public.versions v
-                        SET "content" = jsonb_set("content"::jsonb,
-                            array['pipeline', 'blocks', s_name, 'params', 'mapping', s_map, 'value']::varchar[],
-                            jsonb_build_array(concat(
-                                left(str_to_update, 8),
-                                'application_body.',
-                                substring(str_to_update, 9, length(str_to_update)))) -> 0,
-                            false)
-                        WHERE v.id = input_v_ids;
-                    END IF;
-                END LOOP;
+                        WHERE id = input_v_ids
+                            AND jsonb_typeof(content -> 'pipeline' -> 'blocks' -> s_name -> 'params' -> 'mapping') = 'object'
+                    ),
+                    array['pipeline', 'blocks', s_name, 'params', 'mapping']::varchar[],
+                    input_v_ids
+            );
         END LOOP;
 END $function$;
 
 SELECT add_app_body_prefix_mapping(id)
-FROM pipeliner.public.versions;
+FROM pipeliner.public.versions
+WHERE deleted_at IS NULL
+    AND jsonb_typeof(content -> 'pipeline' -> 'blocks') = 'object';
 
 
 
