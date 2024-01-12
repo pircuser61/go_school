521a968d596428abae6b132c6d498faee3dd70ec
Danil Zotin <dnzotin@mts.ru>
2024-01-12 11:31:06 +0300

    getFucntionById returns only last version. Runs old versions with new contracts. This is a fix of that error.

--- internal/functions/function.go
+++ internal/functions/function.go
@@ -70,6 +70,69 @@ func (s *Service) GetFunction(ctx context.Context, id string) (result Function, 
 	}, nil
 }
 
+func (s *Service) GetFunctionByNameAndVersion(ctx context.Context, name, version string) (result Function, err error) {
+	res, err := s.cli.GetFunctionByNameAndVersion(ctx,
+		&function_v1.GetFunctionByNameAndVersionRequest{
+			FunctionName:    name,
+			FunctionVersion: version,
+		})
+
+	if err != nil {
+		return Function{}, err
+	}
+
+	input, inputConvertErr := convertToParamMetadata(res.Function.Input)
+	if inputConvertErr != nil {
+		return Function{}, inputConvertErr
+	}
+
+	output, outputConvertErr := convertToParamMetadata(res.Function.Output)
+	if outputConvertErr != nil {
+		return Function{}, outputConvertErr
+	}
+
+	var options Options
+	optionsUnmarshalErr := json.Unmarshal([]byte(res.Function.Options), &options)
+	if err != nil {
+		return Function{}, optionsUnmarshalErr
+	}
+
+	versions := make([]Version, 0)
+
+	for _, v := range res.Function.Versions {
+		versions = append(versions, Version{
+			FunctionId:  v.FunctionId,
+			VersionId:   v.VersionId,
+			Description: v.Description,
+			Version:     v.Version,
+			Input:       v.Input,
+			Output:      v.Output,
+			Options:     v.Options,
+			Contracts:   v.Contracts,
+			CreatedAt:   v.CreatedAt,
+			DeletedAt:   v.DeletedAt,
+			UpdatedAt:   v.UpdatedAt,
+		})
+	}
+
+	return Function{
+		FunctionId:  res.Function.FunctionId,
+		VersionId:   res.Function.VersionId,
+		Name:        res.Function.Name,
+		Description: res.Function.Description,
+		Version:     res.Function.Version,
+		Uses:        res.Function.Uses,
+		Input:       input,
+		Output:      output,
+		Options:     options,
+		Contracts:   res.Function.Contracts,
+		CreatedAt:   res.Function.CreatedAt,
+		DeletedAt:   res.Function.DeletedAt,
+		UpdatedAt:   res.Function.UpdatedAt,
+		Versions:    versions,
+	}, nil
+}
+
 func convertToParamMetadata(source string) (result map[string]ParamMetadata, err error) {
 	unmarshalErr := json.Unmarshal([]byte(source), &result)
 	if unmarshalErr != nil {
