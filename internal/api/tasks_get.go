7542c206a434b00528421bdd3ff53bc52d2c2a46
sobugreye1 <sobugreye1@mts.ru>
2024-01-18 10:25:39 +0300

    add sign to Accessible nodes check

--- internal/api/tasks_get.go
+++ internal/api/tasks_get.go
@@ -422,6 +422,7 @@ func (ae *APIEnv) getAccessibleForms(currentUser string, steps *entity.TaskSteps
 		ApproverBlockType  = "approver"
 		ExecutionBlockType = "execution"
 		FormBlockType      = "form"
+		SignBlockType      = "sign"
 	)
 
 	accessibleForms = make(map[string]struct{}, 0)
@@ -513,6 +514,19 @@ func (ae *APIEnv) getAccessibleForms(currentUser string, steps *entity.TaskSteps
 			}
 
 			for _, form := range execution.FormsAccessibility {
+				if form.AccessType != TypeAccessFormNone {
+					accessibleForms[form.NodeId] = struct{}{}
+				}
+			}
+
+		case SignBlockType:
+			var sign pipeline.SignData
+			unmarshalErr := json.Unmarshal(s.State[s.Name], &sign)
+			if unmarshalErr != nil {
+				return nil, unmarshalErr
+			}
+
+			for _, form := range sign.FormsAccessibility {
 				if form.AccessType != TypeAccessFormNone {
 					accessibleForms[form.NodeId] = struct{}{}
 				}
