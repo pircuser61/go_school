c7d9fdf341318c0b95ba55d2401153579d81c7df
Roman <petrinfedor35@gmail.com>
2024-02-19 15:39:49 +0300

    fix migration

--- migrations/20240126122632_alter_works.sql
+++ migrations/20240126122632_alter_works.sql
@@ -6,26 +6,15 @@ ALTER TABLE works
 comment on column works.version_sla_id is 'id sla из настроек процесса';
 
 -- для тех, что были созданы тогда, когда sla уже был
+WITH vsla AS (SELECT id, created_at, version_id FROM version_sla ORDER BY created_at DESC)
 UPDATE works
 SET version_sla_id = (
     SELECT id
-    FROM version_sla
-    WHERE version_id = works.version_id
-      AND created_at <= works.started_at
-    ORDER BY created_at DESC
+    FROM vsla
+    WHERE vsla.version_id = works.version_id
+      AND vsla.created_at <= works.started_at
     LIMIT 1
 );
-
--- для тех, что были созданы задолго до того, когда sla был создан, для них просто берем
-UPDATE works
-SET version_sla_id = (
-    SELECT id
-    FROM version_sla
-    WHERE version_id = works.version_id
-    ORDER BY created_at ASC
-    LIMIT 1
-)
-WHERE version_sla_id is null;
 
 -- +goose StatementEnd
 
