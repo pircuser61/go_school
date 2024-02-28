ef437327f98b5766dc18ba31b95f75b89cb74799
Андрей Нестеров <aenest10@mts.ru>
2024-02-28 13:01:30 +0300

    JAP-4840 fix reentry to parallel execution

---
+++ migrations/20240226165350_add_isactive_to_variable_storage.sql
@@ -0,0 +1,12 @@
+
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE  variable_storage ADD COLUMN IF NOT EXISTS is_active BOOLEAN NOT NULL DEFAULT true;
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE variable_storage
+DROP COLUMN IF EXISTS is_active;
+-- +goose StatementEnd
