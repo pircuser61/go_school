bbc0c2059ce9f2073dcd50f060674a48523099cc
Danil Zotin <dnzotin@mts.ru>
2024-04-01 11:26:56 +0300

    [JAP-5272] code review fix

--- migrations/20240401104918_change_add_approvers_to_common.sql
+++ migrations/20240401104918_change_add_approvers_to_common.sql
@@ -1,13 +1,13 @@
 -- +goose Up
 -- +goose StatementBegin
 UPDATE dict_actions
-SET id = 'common'
+SET node_type = 'common'
 WHERE id = 'add_approvers';
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 UPDATE dict_actions
-SET id = 'add_approvers'
-WHERE id = 'common';
+SET node_type = 'approvement'
+WHERE id = 'add_approvers';
 -- +goose StatementEnd
