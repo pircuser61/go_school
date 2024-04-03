ac7056291260f9f40875b4d810f5171e1209a625
Roman <sitius.ru@gmail.com>
2024-04-03 08:15:06 +0300

    run fineshed tasks

--- internal/pipeline/process_block.go
+++ internal/pipeline/process_block.go
@@ -533,15 +533,6 @@ func ProcessBlockWithEndMapping(
 		return false, nil
 	}
 
-	_, err = runCtx.Services.Storage.CreateTaskEvent(ctx, &entity.CreateTaskEvent{
-		WorkID:    runCtx.TaskID.String(),
-		EventType: "pause",
-		Author:    db.SystemLogin,
-	})
-	if err != nil {
-		return false, err
-	}
-
 	endErr := processBlockEnd(ctx, stringStatus, runCtx)
 	if endErr != nil {
 		log.WithError(endErr).Error("couldn't send process end notification")
