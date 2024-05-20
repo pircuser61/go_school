64bcdaa0a78ccfb0bd467294aa25d6064adb4b7c
Roman <sitius.ru@gmail.com>
2024-05-20 22:25:56 +0300

    fix tests and lint

--- internal/api/controller.go
+++ internal/api/controller.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"context"
-	"gitlab.services.mts.ru/jocasta/pipeliner/internal/integrations"
 	"net/http"
 	"time"
 
@@ -21,6 +20,7 @@ import (
 	"gitlab.services.mts.ru/jocasta/pipeliner/internal/functions"
 	"gitlab.services.mts.ru/jocasta/pipeliner/internal/hrgate"
 	human_tasks "gitlab.services.mts.ru/jocasta/pipeliner/internal/humantasks"
+	"gitlab.services.mts.ru/jocasta/pipeliner/internal/integrations"
 	"gitlab.services.mts.ru/jocasta/pipeliner/internal/kafka"
 	"gitlab.services.mts.ru/jocasta/pipeliner/internal/mail"
 	mail_fetcher "gitlab.services.mts.ru/jocasta/pipeliner/internal/mail/fetcher"
