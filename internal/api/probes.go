d0951f95744dacb5c5c922fe6efec559a9ff6922
Быков Сергей <svbyk11@mts.ru>
2024-07-08 20:33:03 +0400

    [JAP-6060] ping DB when alive probe checking

--- internal/api/probes.go
+++ internal/api/probes.go
@@ -6,7 +6,16 @@ import (
 	"time"
 )
 
-func (ae *Env) Alive(_ http.ResponseWriter, _ *http.Request) {}
+func (ae *Env) Alive(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithDeadline(r.Context(), time.Now().Add(2*time.Second))
+	defer cancel()
+
+	if err := ae.DB.Ping(ctx); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+}
 
 func (ae *Env) Ready(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithDeadline(r.Context(), time.Now().Add(2*time.Second))
