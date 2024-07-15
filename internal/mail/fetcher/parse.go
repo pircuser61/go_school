7ee59d40eed37d8a0297ee98e218652b4e893840
root <sitius.ru@gmail.com>
2024-07-15 23:22:42 +0300

    charset

--- internal/mail/fetcher/parse.go
+++ internal/mail/fetcher/parse.go
@@ -9,6 +9,7 @@ import (
 	"github.com/emersion/go-imap"
 
 	"github.com/emersion/go-message"
+	_ "github.com/emersion/go-message/charset"
 	"github.com/emersion/go-message/mail"
 
 	"github.com/pkg/errors"
