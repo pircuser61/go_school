56a9a2dde207f39808f31a366a16ed96b297ddbb
Поляков Владислав Андреевич <vapoliak@mts.ru>
2024-08-21 14:07:10 +0300

    Revert "Merge branch 'bugfix/JAP-6249' into 'develop'"
    This reverts merge request !1671

--- internal/pipeline/process_block.go
+++ internal/pipeline/process_block.go
@@ -551,63 +551,59 @@ func ProcessBlockWithEndMapping(
 		WithField(script.StepName, name)
 
 	ctx = logger.WithLogger(ctx, log)
-	failedBlock, pErr := processor.ProcessBlock(ctx, 0)
 
+	failedBlock, pErr := processor.ProcessBlock(ctx, 0)
 	if pErr != nil {
 		log.WithError(pErr).Error("couldn't process block with end mapping, ProcessBlock")
 
 		return failedBlock, false, pErr
 	}
 
-	go func() {
-		updDeadlineErr := processor.updateTaskExecDeadline(ctx)
+	updDeadlineErr := processor.updateTaskExecDeadline(ctx)
+	if updDeadlineErr != nil {
+		log.WithError(updDeadlineErr).Error("couldn't update task deadline")
 
-		if updDeadlineErr != nil {
-			log.WithError(updDeadlineErr).Error("couldn't update task deadline")
+		return "", false, updDeadlineErr
+	}
 
-			return
+	intStatus, stringStatus, err := runCtx.Services.Storage.GetTaskStatusWithReadableString(ctx, runCtx.TaskID)
+	if err != nil {
+		log.WithError(err).Error("couldn't get task status after processing")
+
+		return "", false, nil
+	}
+
+	if intStatus != db.RunStatusFinished && intStatus != db.RunStatusStopped {
+		return "", false, nil
+	}
+
+	if intStatus == db.RunStatusFinished && statusBefore != db.RunStatusFinished {
+		params := struct {
+			Steps []string `json:"steps"`
+		}{Steps: []string{}}
+
+		jsonParams, mrshErr := json.Marshal(params)
+		if mrshErr != nil {
+			log.Error(mrshErr)
 		}
 
-		intStatus, stringStatus, err := runCtx.Services.Storage.GetTaskStatusWithReadableString(ctx, runCtx.TaskID)
+		_, err = runCtx.Services.Storage.CreateTaskEvent(ctx, &entity.CreateTaskEvent{
+			WorkID:    runCtx.TaskID.String(),
+			EventType: "pause",
+			Author:    db.SystemLogin,
+			Params:    jsonParams,
+		})
 		if err != nil {
-			log.WithError(err).Error("couldn't get task status after processing")
+			log.WithError(updDeadlineErr).Error("couldn't create task event")
 
-			return
+			return "", false, err
 		}
+	}
 
-		if intStatus != db.RunStatusFinished && intStatus != db.RunStatusStopped {
-			return
-		}
-
-		if intStatus == db.RunStatusFinished && statusBefore != db.RunStatusFinished {
-			params := struct {
-				Steps []string `json:"steps"`
-			}{Steps: []string{}}
-
-			jsonParams, mrshErr := json.Marshal(params)
-			if mrshErr != nil {
-				log.Error(mrshErr)
-			}
-
-			_, err = runCtx.Services.Storage.CreateTaskEvent(ctx, &entity.CreateTaskEvent{
-				WorkID:    runCtx.TaskID.String(),
-				EventType: "pause",
-				Author:    db.SystemLogin,
-				Params:    jsonParams,
-			})
-			if err != nil {
-				log.WithError(updDeadlineErr).Error("couldn't create task event")
-
-				return
-			}
-		}
-
-		endErr := processBlockEnd(ctx, stringStatus, runCtx)
-
-		if endErr != nil {
-			log.WithError(endErr).Error("couldn't send process end notification")
-		}
-	}()
+	endErr := processBlockEnd(ctx, stringStatus, runCtx)
+	if endErr != nil {
+		log.WithError(endErr).Error("couldn't send process end notification")
+	}
 
 	return "", true, nil
 }
