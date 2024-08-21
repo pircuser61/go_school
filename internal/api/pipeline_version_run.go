4ed07c642956e5d7266ef43b93445b0ee25dcd6f
boldyrevn <boldyrevn@mts.ru>
2024-08-21 16:36:11 +0300

    change error handling for rpc

--- internal/api/pipeline_version_run.go
+++ internal/api/pipeline_version_run.go
@@ -543,9 +543,11 @@ func handleLaunchTaskError(ctx c.Context, storage db.Database, taskID uuid.UUID,
 
 	switch {
 	case errorutils.IsRemoteCallError(err):
-		log.WithError(err).Warning("remote call error")
+		log.WithError(err).Error("remote call error")
 
-		return nil
+		_ = storage.UpdateTaskStatus(ctx, taskID, db.RunStatusError, err.Error(), "")
+
+		return err
 	case err != nil:
 		log.WithError(err).Error("process empty task error")
 
