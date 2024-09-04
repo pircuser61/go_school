871690242f25c2f1e9e88eacd5c0b2d608da4985
oleg kurov <kurov.o.y@sbertech.ru>
2024-09-04 16:35:48 +0300

    feat[SDHR-72]: fix linter

--- internal/entity/task.go
+++ internal/entity/task.go
@@ -10,30 +10,30 @@ import (
 )
 
 type Step struct {
-	ID            uuid.UUID                  `json:"-"`
-	WorkID        uuid.UUID                  `json:"work_id"`
-	WorkNumber    string                     `json:"work_number"`
-	Time          time.Time                  `json:"time"`
-	Type          string                     `json:"type"`
-	Name          string                     `json:"name"`
-	State         map[string]json.RawMessage `json:"state" swaggertype:"object"`
-	Storage       map[string]interface{}     `json:"storage"`
-	Errors        []string                   `json:"errors"`
-	Steps         []string                   `json:"steps"`
-	BreakPoints   []string                   `json:"-"`
-	HasError      bool                       `json:"has_error"`
-	Status        string                     `json:"status"`
-	Initiator     string                     `json:"initiator"`
-	UpdatedAt     *time.Time                 `json:"updated_at"`
-	IsTest        bool                       `json:"-"`
-	ShortTitle    *string                    `json:"short_title,omitempty"`
-	Attachments   int                        `json:"attachments"`
-	IsPaused      bool                       `json:"is_paused"`
-	People        []string                   `json:"people"`
-	GroupID       string                     `json:"group_id"`
-	GroupName     string                     `json:"group_name"`
-	GroupLimit    int                        `json:"group_limit"`
-	InitialPeople []string                   `json:"initial_people"`
+	ID                  uuid.UUID                  `json:"-"`
+	WorkID              uuid.UUID                  `json:"work_id"`
+	WorkNumber          string                     `json:"work_number"`
+	Time                time.Time                  `json:"time"`
+	Type                string                     `json:"type"`
+	Name                string                     `json:"name"`
+	State               map[string]json.RawMessage `json:"state" swaggertype:"object"`
+	Storage             map[string]interface{}     `json:"storage"`
+	Errors              []string                   `json:"errors"`
+	Steps               []string                   `json:"steps"`
+	BreakPoints         []string                   `json:"-"`
+	HasError            bool                       `json:"has_error"`
+	Status              string                     `json:"status"`
+	Initiator           string                     `json:"initiator"`
+	UpdatedAt           *time.Time                 `json:"updated_at"`
+	IsTest              bool                       `json:"-"`
+	ShortTitle          *string                    `json:"short_title,omitempty"`
+	Attachments         int                        `json:"attachments"`
+	IsPaused            bool                       `json:"is_paused"`
+	People              []string                   `json:"people"`
+	GroupID             string                     `json:"group_id"`
+	GroupName           string                     `json:"group_name"`
+	GroupLimit          int                        `json:"group_limit"`
+	InitialPeople       []string                   `json:"initial_people"`
 	Content             map[string]json.RawMessage `json:"content" swaggertype:"object"`
 	CurrentExecutorData map[string]json.RawMessage `json:"current_executor_data" swaggertype:"object"`
 }
